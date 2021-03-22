9db41260bfd6819f2657ec2b5acd5c1926fdef12
DevyaniSehrawat16 <devyani.sehrawat02@gmail.com>
2021-03-22 18:14:51 +0530

    first commit

---
+++ src/test/java/qaMavenJava/MavenJava/AppiumTest.java
@@ -0,0 +1,13 @@
+package qaMavenJava.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void c()
+	{
+		System.out.println("sandhya");
+	}
+
+}
---
+++ src/test/java/qaMavenJava/MavenJava/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaMavenJava.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void a()
+	{
+	System.out.println("devyani");	
+	}
+	
+	@Test
+	public void b()
+	{
+		System.out.println("rahul");
+	}
+
+}
